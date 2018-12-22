7a704c090e91cba9bb44fb90032f074f686ed59d
Andrew Rosenberg <andrew@intsite.com>
2018-12-21 17:40:40 -0800

    Now with live streaming

--- src/main/scala/SequenceFinder.scala
+++ src/main/scala/SequenceFinder.scala
@@ -1,14 +1,16 @@
+import scala.util.Try
 
 object SequenceFinder {
   /**
     * Note that the requirement was for a function nucleotide next(stream *s) but I decided to use Scala's Stream implementation.
-    * @param seq The sequence of nucleotides we are searching for ex: AGCT
-    * @param stream A continuous stream of Nucleotides ending with ε ex: AGGTACACCATTAGε
+    *
+    * @param seq         The sequence of nucleotides we are searching for ex: AGCT
+    * @param stream      A continuous stream of Nucleotides ending with ε ex: AGGTACACCATTAGε
     * @param countBefore the 'x' count of nucleotides before the target
-    * @param countAfter the 'y' count of nucleotides after the target
-    * @return a list of matches. There's a separate function to pretty-print them
+    * @param countAfter  the 'y' count of nucleotides after the target
+    * @return a list of matches. There's a separate function to pretty-print them. Last item will be None if it's non-inifinite
     */
-  def findSequence(seq: Seq[Nucleotide], stream: Stream[Nucleotide], countBefore: Int, countAfter: Int): List[NSequence] = {
+  def findSequence(seq: Seq[Nucleotide], stream: Stream[Nucleotide], countBefore: Int, countAfter: Int): Stream[Option[NSequence]] = {
     var lastX = List[Nucleotide]()
     val iter = stream.iterator
     var candidates = List[NSequence]()
@@ -16,72 +18,95 @@ object SequenceFinder {
     // should speed up access if it's an array
     val seqArr = seq.toArray
 
-    iter
-      .takeWhile(_ != ε)
-      .foreach { nuc =>
-        if (nuc == seqArr.head) {
-          // We'll start matching this sequence.
-          candidates = NSequence(lastX.reverse) :: candidates
-        }
+    // Using this as an ID for the candidate
+    var counter = 0L
 
-        // Walk through all candidate sequences and see which ones need to be updated.
-        candidates = candidates map {
-          case ns if ns.isComplete ⇒ ns // ignore
-          case ns if ns.isMatched ⇒ ns.copy(after = nuc :: ns.after, isComplete = ns.after.length + 1 >= countAfter) // either now complete or we're looking for the after nucs
-          case ns if nuc == seqArr(ns.current) ⇒
-            if (ns.current + 1 == seqArr.length)
-              ns.copy(isMatched = true) // We have a winner!
-            else
-              ns.copy(current = ns.current + 1) // Next nuc checks out, keep matching
-          case ns ⇒ ns.copy(isFailed = true) // no match :(
-        } filterNot {
-          _.isFailed // drop the unmatched sequences
-        }
+    Stream.continually[Option[NSequence]] {
+      Try(iter
+        .takeWhile(_ != ε)
+        .foreach { nuc =>
+          if (nuc == seqArr.head) {
+            // We'll start matching this sequence.
+            candidates = NSequence(before = lastX.reverse, start = counter) :: candidates
+          }
+          counter = counter + 1
+          // Walk through all candidate sequences and see which ones need to be updated.
+          candidates = candidates map {
+            case ns if ns.isComplete ⇒ ns // ignore
+            case ns if ns.isMatched ⇒ ns.copy(after = nuc :: ns.after, isComplete = ns.after.length + 1 >= countAfter) // either now complete or we're looking for the after nucs
+            case ns if nuc == seqArr(ns.current) ⇒
+              if (ns.current + 1 == seqArr.length)
+                ns.copy(isMatched = true) // We have a winner!
+              else
+                ns.copy(current = ns.current + 1) // Next nuc checks out, keep matching
+            case ns ⇒ ns.copy(isFailed = true) // no match :(
+          } filterNot {
+            _.isFailed // drop the unmatched sequences
+          }
 
-        // Keep track of the x "before" nucs
-        if (countBefore > 0) {
-          lastX = nuc :: lastX take countBefore
-        }
-      }
+          // Keep track of the x "before" nucs
+          if (countBefore > 0) {
+            lastX = nuc :: lastX take countBefore
+          }
+          // Break the inner loop to output a candidate
+          if (candidates.exists(candidate => candidate.isComplete)) throw new Exception("Found One")
+        })
 
-    // At the end, only look at what was actually matched. Their "after" list needs to be reversed and then the whole list needs to be reversed.
-    candidates
-      .filter(ns ⇒ ns.isMatched)
-      .map(ns ⇒ ns.copy(after = ns.after.reverse))
-      .reverse
+      Option(
+        candidates.collectFirst {
+          case ns@NSequence(_, _, _, _, true, false, _) =>
+            // These are "complete" matches
+            candidates = candidates.filterNot(n => n.start == ns.start)
+            ns.copy(after = ns.after.reverse)
+        } getOrElse {
+          candidates.filter(ns ⇒ ns.isMatched).reverse.collectFirst {
+            case ns@NSequence(_, _, _, true, false, false, _) =>
+              // If there are no more complete matches, this means we're at the end of the stream. Will choose one match on each iteration to throw into the stream
+              candidates = candidates.filterNot(n => n.start == ns.start)
+              ns.copy(after = ns.after.reverse)
+          }.orNull
+        }
+      )
+    }
   }
 
   /**
     * Produces results like "AGCT AAB GTCA" for each item in the list
+    *
     * @param res raw results
     * @param seq the sequence we searched for
     * @return formatted string
     */
-  def formatResults(res: List[NSequence], seq: Seq[Nucleotide]): String = {
+  def dumpStream(stream: Stream[Option[NSequence]], seq: Seq[Nucleotide]): String = {
     val sb = new StringBuilder()
-    res.foreach(ns ⇒ {
-      ns.before.foreach(nuc ⇒ sb.append(nuc))
-      sb.append(' ')
-      seq.foreach(nuc ⇒ sb.append(nuc))
-      sb.append(' ')
-      ns.after.foreach(nuc ⇒ sb.append(nuc))
-      sb.append('\n')
+    stream.takeWhile(_.isDefined).foreach(n => {
+      formatResult(seq, sb, n.get)
+      System.out.println(sb.toString())
     })
+    sb.toString()
+  }
 
-    val result = sb.toString()
-    System.out.println(result)
-    result
+  def formatResult(seq: Seq[Nucleotide], sb: StringBuilder, ns: NSequence): StringBuilder = {
+    ns.before.foreach(nuc ⇒ sb.append(nuc))
+    sb.append(' ')
+    seq.foreach(nuc ⇒ sb.append(nuc))
+    sb.append(' ')
+    ns.after.foreach(nuc ⇒ sb.append(nuc))
+    sb.append('\n')
   }
 }
 
+
 /**
   * Our immutable data object
-  * @param before All the nucleotides before the match up to x
-  * @param after All the nucleotides after the match up to y
-  * @param current Index of item we're looking for
-  * @param isMatched True if we've found the whole match
+  *
+  * @param before     All the nucleotides before the match up to x
+  * @param after      All the nucleotides after the match up to y
+  * @param current    Index of item we're looking for
+  * @param isMatched  True if we've found the whole match
   * @param isComplete True if we've completed the after list
-  * @param isFailed There is no match
+  * @param isFailed   There is no match
+  * @param start      A unique ID for manipulating lists of NSequence
   */
 case class NSequence(
                       before: Seq[Nucleotide],
@@ -90,5 +115,6 @@ case class NSequence(
                       isMatched: Boolean = false, // Are we still matching?
                       isComplete: Boolean = false, // Do we have the after seq complete?
                       isFailed: Boolean = false,
+                      start: Long
                     )
 
--- src/test/scala/HasOffersTest.scala
+++ src/test/scala/HasOffersTest.scala
@@ -3,7 +3,6 @@ import org.scalatest.{FlatSpec, Matchers}
 import scala.util.Random
 
 
-
 class HasOffersTest extends FlatSpec with Matchers {
 
   import SequenceFinder._
@@ -13,13 +12,10 @@ class HasOffersTest extends FlatSpec with Matchers {
     val streamer = testSequence.toStream
 
     val nukeStream: Stream[Nucleotide] = List(A, C, G, T, A, C, C, G, T, ε).toStream
-
-
     val t = findSequence(testSequence, nukeStream, 2, 3)
 
-    System.out.println(s"t=$t")
-
-    formatResults(t, testSequence)
+    val results = dumpStream(t, testSequence)
+    results should be("A CG TAC\nAC CG T\n")
 
   }
 
@@ -27,17 +23,16 @@ class HasOffersTest extends FlatSpec with Matchers {
     val testStream: Stream[Nucleotide] = List(A, A, G, T, A, C, G, T, G, C, A, G, T, G, A, G, T, A, G, T, A, G, A, C, C, T, G, A, C, G, T, A, G, A, C, C, G, A, T, A, T, A, A, G, T, A, G, C, T, A, ε).toStream
     val testSequence = List(A, G, T, A)
     val t = findSequence(testSequence, testStream, 5, 7)
-
-    formatResults(t, testSequence)
+    val results = dumpStream(t, testSequence)
+    results should be("A AGTA CGTGCAG\nCAGTG AGTA GTAGACC\nTGAGT AGTA GACCTGA\nATATA AGTA GCTA\n")
   }
-
 
   "empty stream" should "return empty" in {
     val testStream: Stream[Nucleotide] = List(ε).toStream
     val testSequence = List(A, G, T, A)
     val t = findSequence(testSequence, testStream, 5, 7)
 
-    val results = formatResults(t, testSequence)
+    val results = dumpStream(t, testSequence)
     results should be("")
   }
 
@@ -46,7 +41,7 @@ class HasOffersTest extends FlatSpec with Matchers {
     val testSequence = List(G)
     val t = findSequence(testSequence, testStream, 5, 7)
 
-    val results = formatResults(t, testSequence)
+    val results = dumpStream(t, testSequence)
     results should be("")
   }
 
@@ -55,7 +50,7 @@ class HasOffersTest extends FlatSpec with Matchers {
     val testSequence = List(A)
     val t = findSequence(testSequence, testStream, 5, 7)
 
-    val results = formatResults(t, testSequence)
+    val results = dumpStream(t, testSequence)
     results should be(" A \n")
   }
 
@@ -64,7 +59,7 @@ class HasOffersTest extends FlatSpec with Matchers {
     val testSequence = List(A)
     val t = findSequence(testSequence, testStream, 5, 7)
 
-    val results = formatResults(t, testSequence)
+    val results = dumpStream(t, testSequence)
     results should be(
       " A AAAA\nA A AAA\nAA A AA\nAAA A A\nAAAA A \n"
     )
@@ -75,21 +70,21 @@ class HasOffersTest extends FlatSpec with Matchers {
     val testSequence = List(A, A)
     val t = findSequence(testSequence, testStream, 1, 1)
 
-    val results = formatResults(t, testSequence)
+    val results = dumpStream(t, testSequence)
     results should be(
       " AA A\nA AA A\nA AA A\nA AA A\nA AA A\nA AA A\nA AA A\nA AA A\nA AA \n"
     )
   }
 
-  "extreme test" should "not error out" in {
-    val size = 100000
+  "infinte test" should "not error out" ignore {
+
     var count = 0
     val bigStream = Stream.continually(Random.nextInt(4) match {
       case 0 => A
       case 1 => C
       case 2 => G
       case 3 => T
-    }) take size append Stream(ε)
+    })
 
     count = 0
     val testSequence = Stream.continually(Random.nextInt(4) match {
@@ -100,12 +95,10 @@ class HasOffersTest extends FlatSpec with Matchers {
     }) take 4
     val t = findSequence(testSequence, bigStream, 20, 20)
 
-    val results = formatResults(t, testSequence)
-    System.out.println(s"Big Test:\n$results")
-
-
+    t.foreach(n => {
+      val sb = new StringBuilder()
+      formatResult(testSequence, sb, n.get)
+      System.out.println(sb.toString())
+    })
   }
 }
-
-
-
